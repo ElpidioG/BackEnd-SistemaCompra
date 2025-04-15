c45fbdd160234210b20a005cf534702970befbeb
Elpidio Guillermo Marquez Almanzar <e.marquez1@unapec.edu.do>
2025-04-15 13:35:39 -0400

    ca

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-
-using BackEnd_SistemaCompra.Contexts;
+﻿using BackEnd_SistemaCompra.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models; // <-- Asegúrate de tener este using
 
 namespace BackEnd_SistemaCompra
 {
@@ -10,43 +10,42 @@ namespace BackEnd_SistemaCompra
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddHttpClient();
-            // Add services to the container.
-            builder.Services.AddDbContext<ConexionDB>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
+
+            builder.Services.AddDbContext<ConexionDB>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSql")));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            // ✅ Agrega esta configuración explícita
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API Sistema de Compras",
+                    Version = "v1"
+                });
+            });
 
             builder.Services.AddCors(options =>
             {
-                options.AddPolicy("AllowAllOrigins",
-                    builder =>
-                    {
-                        builder.AllowAnyOrigin()
-                               .AllowAnyMethod()
-                               .AllowAnyHeader();
-                    });
+                options.AddPolicy("AllowAllOrigins", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
             });
 
             var app = builder.Build();
 
-            // Para que permita interactuar con el frontens
-
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
             app.UseCors("AllowAllOrigins");
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
